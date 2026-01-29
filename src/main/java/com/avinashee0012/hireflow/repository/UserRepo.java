04399e7c51aa6a95fb95c638eb21a0d1cb6ee8e9
avinashee0012 <avinashee0012@gmail.com>
2026-01-29 17:02:12 +0530

    feat: add UserRepo and RoleRepo

---
+++ src/main/java/com/avinashee0012/hireflow/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.avinashee0012.hireflow.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.avinashee0012.hireflow.domain.entity.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long>{
+}
