cfd5ceea901bf45a465653c9e105e246c64323f2
avinashee0012 <avinashee0012@gmail.com>
2026-02-01 21:29:01 +0530

    fix: typing error resolved

--- src/main/java/com/avinashee0012/hireflow/exception/CustomUnauthorizedEntityActionException.java
+++ src/main/java/com/avinashee0012/hireflow/exception/CustomUnauthorizedEntityActionException.java
@@ -2,6 +2,6 @@ package com.avinashee0012.hireflow.exception;
 
 public class CustomUnauthorizedEntityActionException extends RuntimeException{
     public CustomUnauthorizedEntityActionException(String message){
-        super("Unauthorize action: " + message);
+        super("Unauthorized action: " + message);
     }
 }
