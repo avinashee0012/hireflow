4b3cd56af7f80633b41dfdff5c78f548f40f9af7
avinashee0012 <avinashee0012@gmail.com>
2026-02-07 18:51:05 +0530

    feat: add application repo methods

---
+++ src/main/java/com/avinashee0012/hireflow/repository/ApplicationRepo.java
@@ -0,0 +1,17 @@
+package com.avinashee0012.hireflow.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.avinashee0012.hireflow.domain.entity.Application;
+
+@Repository
+public interface ApplicationRepo extends JpaRepository<Application, Long>{
+    boolean existsByJobIdAndCandidateId(Long jobId, Long candidateId);
+    Optional<Application> findByIdAndCandidateId(Long applicationId, Long candidateId);
+    Optional<Application> findByIdAndJobIdAndJobAssignedRecruiterId(Long applicationId, Long jobId, Long recruiterId);
+    Optional<Application> findByIdAndOrganisationId(Long applicationId, Long organisationId);
+}
+
