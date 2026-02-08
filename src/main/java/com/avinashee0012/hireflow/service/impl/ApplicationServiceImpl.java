56a9d8b0b3e384b8fc07443a0401506163740c1d
avinashee0012 <avinashee0012@gmail.com>
2026-02-08 13:42:28 +0530

    feat: implement job and application listing service methods

--- src/main/java/com/avinashee0012/hireflow/service/impl/ApplicationServiceImpl.java
+++ src/main/java/com/avinashee0012/hireflow/service/impl/ApplicationServiceImpl.java
@@ -1,5 +1,9 @@
 package com.avinashee0012.hireflow.service.impl;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.avinashee0012.hireflow.config.security.CurrentUserProvider;
@@ -12,6 +16,7 @@ import com.avinashee0012.hireflow.dto.response.ApplicationResponseDto;
 import com.avinashee0012.hireflow.exception.CustomDuplicateEntityException;
 import com.avinashee0012.hireflow.exception.CustomUnauthorizedEntityActionException;
 import com.avinashee0012.hireflow.exception.CustomUnauthorizedException;
+import com.avinashee0012.hireflow.mapper.ApplicationMapper;
 import com.avinashee0012.hireflow.repository.ApplicationRepo;
 import com.avinashee0012.hireflow.repository.JobRepo;
 import com.avinashee0012.hireflow.service.ApplicationService;
@@ -22,52 +27,60 @@ import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class ApplicationServiceImpl implements ApplicationService {
-    
+public class ApplicationServiceImpl implements ApplicationService{
+
+    private static int PAGE_SIZE = 10;
+
     private final ApplicationRepo applicationRepo;
     private final JobRepo jobRepo;
     private final CurrentUserProvider currentUserProvider;
+    private final ApplicationMapper applicationMapper;
 
     @Override
-    @Transactional
-    public ApplicationResponseDto apply(Long jobId) {
+    @Transactional public ApplicationResponseDto apply(Long jobId){
         User user = currentUserProvider.getAuthenticatedUser();
         boolean allowed = user.hasRole("CANDIDATE");
         if (!allowed)
             throw new CustomUnauthorizedException("Only CANDIDATE can apply to jobs");
-        Job job = jobRepo.findById(jobId).orElseThrow(() -> new EntityNotFoundException("Job not found with id: " + jobId));
-        if(job.getJobStatus() != JobStatus.OPEN) throw new IllegalStateException("Can only apply to OPEN jobs");
+        Job job = jobRepo.findById(jobId)
+                .orElseThrow(() -> new EntityNotFoundException("Job not found with id: " + jobId));
+        if (job.getJobStatus() != JobStatus.OPEN)
+            throw new IllegalStateException("Can only apply to OPEN jobs");
         boolean alreadyApplied = applicationRepo.existsByJobIdAndCandidateId(jobId, user.getId());
-        if(alreadyApplied) throw new CustomDuplicateEntityException("Already applied to this job");
+        if (alreadyApplied)
+            throw new CustomDuplicateEntityException("Already applied to this job");
         Application application = new Application(jobId, user.getId(), job.getOrganisationId());
         Application savedApplication = applicationRepo.save(application);
         return new ApplicationResponseDto(savedApplication.getId(), jobId, savedApplication.getApplicationStatus());
     }
 
     @Override
-    @Transactional
-    public void withdraw(Long applicationId) {
+    @Transactional public void withdraw(Long applicationId){
         User user = currentUserProvider.getAuthenticatedUser();
         boolean allowed = user.hasRole("CANDIDATE");
         if (!allowed)
             throw new CustomUnauthorizedException("Only CANDIDATE can withdraw their application");
-        Application application = applicationRepo.findByIdAndCandidateId(applicationId, user.getId()).orElseThrow(() -> new EntityNotFoundException("Application not found with id " + applicationId + " for this user"));
+        Application application = applicationRepo.findByIdAndCandidateId(applicationId, user.getId()).orElseThrow(
+                () -> new EntityNotFoundException("Application not found with id " + applicationId + " for this user"));
         application.withdraw();
     }
 
     @Override
-    @Transactional
-    public ApplicationResponseDto updateStatus(Long applicationId, ApplicationStatusUpdateRequestDto request) {
+    @Transactional public ApplicationResponseDto updateStatus(Long applicationId,
+            ApplicationStatusUpdateRequestDto request){
         User user = currentUserProvider.getAuthenticatedUser();
         boolean allowed = user.hasRole("RECRUITER") || user.hasRole("ORGADMIN");
         if (!allowed)
             throw new CustomUnauthorizedException("Only RECRUITER or ORGADMIN can update status");
         Application application;
-        if(user.hasRole("ORGADMIN")){ // ORGADMIN can update any application
-            application = applicationRepo.findByIdAndOrganisationId(applicationId, user.getOrganisationId()).orElseThrow(() -> new CustomUnauthorizedEntityActionException("Application does not belong to organisation"));
-        } else { // RECRUITER can only update applications for their own job
-            application = applicationRepo.findById(applicationId).orElseThrow(() -> new EntityNotFoundException("Application not found with id: " + applicationId));
-            if(!jobRepo.existsByIdAndAssignedRecruiterId(application.getJobId(), user.getId())){
+        if (user.hasRole("ORGADMIN")){ // ORGADMIN can update any application
+            application = applicationRepo.findByIdAndOrganisationId(applicationId, user.getOrganisationId())
+                    .orElseThrow(() -> new CustomUnauthorizedEntityActionException(
+                            "Application does not belong to organisation"));
+        } else{ // RECRUITER can only update applications for their own job
+            application = applicationRepo.findById(applicationId)
+                    .orElseThrow(() -> new EntityNotFoundException("Application not found with id: " + applicationId));
+            if (!jobRepo.existsByIdAndAssignedRecruiterId(application.getJobId(), user.getId())){
                 throw new CustomUnauthorizedEntityActionException("Application not associated with recruiter");
             }
         }
@@ -82,6 +95,25 @@ public class ApplicationServiceImpl implements ApplicationService {
                 throw new CustomUnauthorizedEntityActionException("Invalid transition status");
         }
         return new ApplicationResponseDto(applicationId, application.getJobId(), application.getApplicationStatus());
+    }
+
+    @Override public Page<ApplicationResponseDto> getApplications(int page, String sortBy, String direction){
+        User user = currentUserProvider.getAuthenticatedUser();
+        Sort sort = direction.equalsIgnoreCase("asc") ? Sort.by(Sort.Direction.ASC, sortBy)
+                : Sort.by(Sort.Direction.DESC, sortBy);
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE, sort);
+        Page<Application> applications;
+        if (user.hasRole("CANDIDATE")){
+            applications = applicationRepo.findByCandidateId(user.getId(), pageable);
+        } else if (user.hasRole("RECRUITER")){
+            applications = applicationRepo.findByJobIdIn(jobRepo.findJobIdsByAssignedRecruiterId(user.getId()),
+                    pageable);
+        } else if (user.hasRole("ORGADMIN")){
+            applications = applicationRepo.findByOrganisationId(user.getOrganisationId(), pageable);
+        } else{
+            throw new CustomUnauthorizedException("Access denied");
+        }
+        return applications.map(applicationMapper::toResponse);
     }
 
 }
--- src/main/java/com/avinashee0012/hireflow/service/impl/JobServiceImpl.java
+++ src/main/java/com/avinashee0012/hireflow/service/impl/JobServiceImpl.java
@@ -1,11 +1,16 @@
 package com.avinashee0012.hireflow.service.impl;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.avinashee0012.hireflow.config.security.CurrentUserProvider;
 import com.avinashee0012.hireflow.domain.entity.Job;
 import com.avinashee0012.hireflow.domain.entity.User;
+import com.avinashee0012.hireflow.domain.enums.JobStatus;
 import com.avinashee0012.hireflow.dto.request.JobRequestDto;
 import com.avinashee0012.hireflow.dto.response.JobResponseDto;
 import com.avinashee0012.hireflow.exception.CustomUnauthorizedEntityActionException;
@@ -19,15 +24,16 @@ import lombok.AllArgsConstructor;
 
 @Repository
 @AllArgsConstructor
-public class JobServiceImpl implements JobService {
+public class JobServiceImpl implements JobService{
+
+    private static int PAGE_SIZE = 10;
 
     private final JobRepo jobRepo;
     private final CurrentUserProvider currentUserProvider;
     private final JobMapper jobMapper;
 
     @Override
-    @Transactional
-    public JobResponseDto createJob(JobRequestDto request) {
+    @Transactional public JobResponseDto createJob(JobRequestDto request){
         User user = currentUserProvider.getAuthenticatedUser();
         boolean allowed = user.hasRole("RECRUITER") || user.hasRole("ORGADMIN");
         if (!allowed)
@@ -37,8 +43,7 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
-    @Transactional
-    public void closeJob(Long jobId) {
+    @Transactional public void closeJob(Long jobId){
         User user = currentUserProvider.getAuthenticatedUser();
         Job job = jobRepo.findByIdAndAssignedRecruiterId(jobId, user.getId())
                 .orElseThrow(() -> new CustomUnauthorizedEntityActionException("Job not assigned to this recruiter"));
@@ -46,8 +51,7 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
-    @Transactional
-    public void reopenJob(Long jobId) {
+    @Transactional public void reopenJob(Long jobId){
         User user = currentUserProvider.getAuthenticatedUser();
         if (!user.hasRole("ORGADMIN"))
             throw new CustomUnauthorizedException("Only ORGADMIN can reopen jobs");
@@ -56,37 +60,56 @@ public class JobServiceImpl implements JobService {
         job.reopen();
     }
 
-    @Override
-    public JobResponseDto getJobById(Long jobId) {
+    @Override public JobResponseDto getJobById(Long jobId){
         User user = currentUserProvider.getAuthenticatedUser();
         Job job;
-        if (user.hasRole("ORGADMIN")) {
+        if (user.hasRole("ORGADMIN")){
             job = jobRepo.findByIdAndOrganisationId(jobId, user.getOrganisationId())
                     .orElseThrow(() -> new EntityNotFoundException("Job not found in organisation"));
-        } else if (user.hasRole("RECRUITER")) {
+        } else if (user.hasRole("RECRUITER")){
             job = jobRepo.findByIdAndAssignedRecruiterId(jobId, user.getId()).orElseThrow(
                     () -> new CustomUnauthorizedEntityActionException("Job not assigned to this recruiter"));
-        } else {
+        } else{
             throw new CustomUnauthorizedException("Access denied");
         }
         return jobMapper.toResponse(job);
     }
 
     @Override
-    @Transactional
-    public JobResponseDto updateJob(Long jobId, JobRequestDto request) {
+    @Transactional public JobResponseDto updateJob(Long jobId, JobRequestDto request){
         User user = currentUserProvider.getAuthenticatedUser();
         Job job;
-        if (user.hasRole("ORGADMIN")) {
+        if (user.hasRole("ORGADMIN")){
             job = jobRepo.findByIdAndOrganisationId(jobId, user.getOrganisationId())
                     .orElseThrow(() -> new EntityNotFoundException("Job not found in organisation"));
-        } else if (user.hasRole("RECRUITER")) {
+        } else if (user.hasRole("RECRUITER")){
             job = jobRepo.findByIdAndAssignedRecruiterId(jobId, user.getId()).orElseThrow(
                     () -> new CustomUnauthorizedEntityActionException("Job not assigned to this recruiter"));
-        } else {
+        } else{
             throw new CustomUnauthorizedException("Access denied");
         }
         job.updateDetails(request.getTitle(), request.getDescription());
         return jobMapper.toResponse(job);
     }
+
+    @Override public Page<JobResponseDto> getJobs(int page, String sortBy, String direction){
+        User user = currentUserProvider.getAuthenticatedUser();
+        Sort sort = direction.equalsIgnoreCase("asc") ? Sort.by(Sort.Direction.ASC, sortBy)
+                : Sort.by(Sort.Direction.DESC, sortBy);
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE, sort);
+        Page<Job> jobs;
+        if (user.hasRole("RECRUITER")){
+            jobs = jobRepo.findByAssignedRecruiterId(user.getId(), pageable);
+        } else if (user.hasRole("ORGADMIN")){
+            jobs = jobRepo.findByOrganisationId(user.getOrganisationId(), pageable);
+        } else if (user.hasRole("CANDIDATE")){
+            jobs = jobRepo.findByJobStatus(JobStatus.OPEN, pageable);
+        } else if (user.hasRole("SUPPORT")){
+            jobs = jobRepo.findByOrganisationId(user.getOrganisationId(), pageable);
+        } else{
+            throw new CustomUnauthorizedException("Access denied");
+        }
+        return jobs.map(jobMapper::toResponse);
+    }
+
 }
