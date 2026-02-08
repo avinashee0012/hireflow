34f97cf59f9279f9f74f398bd287cf60923ad8bb
avinashee0012 <avinashee0012@gmail.com>
2026-02-08 13:41:28 +0530

    feat: add job and application repo method for listing endpoints

--- src/main/java/com/avinashee0012/hireflow/repository/JobRepo.java
+++ src/main/java/com/avinashee0012/hireflow/repository/JobRepo.java
@@ -1,15 +1,29 @@
 package com.avinashee0012.hireflow.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.avinashee0012.hireflow.domain.entity.Job;
+import com.avinashee0012.hireflow.domain.enums.JobStatus;
 
 @Repository
-public interface JobRepo extends JpaRepository<Job, Long> {
+public interface JobRepo extends JpaRepository<Job, Long>{
     Optional<Job> findByIdAndOrganisationId(Long jobId, Long organisationId);
+
     boolean existsByIdAndAssignedRecruiterId(Long jobId, Long recruiterId);
+
     Optional<Job> findByIdAndAssignedRecruiterId(Long jobId, Long recruiterId);
+
+    Page<Job> findByAssignedRecruiterId(Long recruiterId, Pageable pageable);
+
+    Page<Job> findByOrganisationId(Long organisationId, Pageable pageable);
+
+    Page<Job> findByJobStatus(JobStatus status, Pageable pageable);
+
+    List<Long> findJobIdsByAssignedRecruiterId(Long recruiterId);
 }
