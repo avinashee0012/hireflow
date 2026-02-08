6adcdd50b462c9e6aa3c6c293ff692c1596646ce
avinashee0012 <avinashee0012@gmail.com>
2026-02-08 13:39:16 +0530

    feat: add job service contracts

--- src/main/java/com/avinashee0012/hireflow/service/JobService.java
+++ src/main/java/com/avinashee0012/hireflow/service/JobService.java
@@ -1,12 +1,20 @@
 package com.avinashee0012.hireflow.service;
 
+import org.springframework.data.domain.Page;
+
 import com.avinashee0012.hireflow.dto.request.JobRequestDto;
 import com.avinashee0012.hireflow.dto.response.JobResponseDto;
 
-public interface JobService {
+public interface JobService{
     JobResponseDto createJob(JobRequestDto request);
+
     JobResponseDto getJobById(Long jobId);
+
     JobResponseDto updateJob(Long jobId, JobRequestDto request);
+
     void closeJob(Long jobId);
+
     void reopenJob(Long jobId);
+
+    Page<JobResponseDto> getJobs(int page, String sortby, String direction);
 }
