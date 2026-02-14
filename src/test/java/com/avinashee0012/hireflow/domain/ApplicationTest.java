f129d19ead043527e221a05eb5f3e52c749ce6ad
avinashee0012 <avinashee0012@gmail.com>
2026-02-14 18:57:13 +0530

    feat: add application tests

--- src/test/java/com/avinashee0012/hireflow/domain/ApplicationTest.java
+++ src/test/java/com/avinashee0012/hireflow/domain/ApplicationTest.java
@@ -2,24 +2,73 @@ package com.avinashee0012.hireflow.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.avinashee0012.hireflow.domain.entity.Application;
 import com.avinashee0012.hireflow.domain.enums.ApplicationStatus;
+import com.avinashee0012.hireflow.exception.CustomUnauthorizedEntityActionException;
 
 public class ApplicationTest {
 
+    private Application application;
+
+    @BeforeEach
+    void setup(){
+        application = new Application(1L, 2L, 3L);
+    }
+
+    @Test
+    void shouldCreateApplicationWithAppliedStatus() {
+        assertEquals(ApplicationStatus.APPLIED,
+                application.getApplicationStatus());
+    }
+
     @Test
     void shouldShortlistApplication() {
-        Application app = new Application(1L, 2L, 3L);
-        app.shortlist();
-        assertEquals(ApplicationStatus.SHORTLISTED, app.getApplicationStatus());
+        application.shortlist();
+        assertEquals(ApplicationStatus.SHORTLISTED, application.getApplicationStatus());
     }
 
     @Test
     void shouldRejectApplication() {
-        Application app = new Application(1L, 2L, 3L);
-        app.reject();
-        assertEquals(ApplicationStatus.REJECTED, app.getApplicationStatus());
+        application.reject();
+        assertEquals(ApplicationStatus.REJECTED, application.getApplicationStatus());
+    }
+
+    @Test
+    void shouldWithdrawApplication() {
+        application.withdraw();
+        assertEquals(ApplicationStatus.WITHDRAWN, application.getApplicationStatus());
+    }
+
+    @Test
+    void shouldNotWithdrawTwice() {
+        application.withdraw();
+        assertThrows(CustomUnauthorizedEntityActionException.class, application::withdraw);
+    }
+
+    @Test
+    void shouldNotShortlistWithdrawnApplication() {
+        application.withdraw();
+        assertThrows(CustomUnauthorizedEntityActionException.class, application::shortlist);
+    }
+
+    @Test
+    void shouldNotRejectAlreadyRejectedApplication() {
+        application.reject();
+        assertThrows(CustomUnauthorizedEntityActionException.class, application::reject);
+    }
+
+    @Test
+    void shouldNotModifyAfterShortlisted() {
+        application.shortlist();
+        assertThrows(CustomUnauthorizedEntityActionException.class, application::withdraw);
+    }
+
+    @Test
+    void shouldNotModifyAfterRejected() {
+        application.reject();
+        assertThrows(CustomUnauthorizedEntityActionException.class, application::shortlist);
     }
 }
