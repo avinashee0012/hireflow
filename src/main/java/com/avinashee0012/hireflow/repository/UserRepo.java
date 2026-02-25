265024b5a88a64e2d1d862f8757ddf4db907ac24
avinashee0012 <avinashee0012@gmail.com>
2026-02-25 11:14:43 +0530

    feat: add method to user repo

--- src/main/java/com/avinashee0012/hireflow/repository/UserRepo.java
+++ src/main/java/com/avinashee0012/hireflow/repository/UserRepo.java
@@ -2,6 +2,8 @@ package com.avinashee0012.hireflow.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,5 @@ import com.avinashee0012.hireflow.domain.entity.User;
 public interface UserRepo extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    Page<User> findByOrganisationId(Long organisationId, PageRequest pageRequest);
 }
